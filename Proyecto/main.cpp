d8d15d7227a3d444ccd84b22c1bdf54be2a8a851
A01710217 <111156446+A01710217@users.noreply.github.com>
2022-11-10 22:23:47 -0600

    Update main.cpp

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,1 +1,23 @@
+/*
+Juego de Cartas.
+*/
 
+#include <iostream>
+using namespace std;
+
+#include "Criaturas.h"
+#include "Estructuras.h"
+#include "Hechisos.h"
+
+int main() {
+    Criaturas criatura1 = Criaturas("Yermo", "Dave el muerto", 25);
+    criatura1.invocar();
+
+    Estructuras estructura1 = Estructuras("Tierras boscosas", "Fuerte siempre verde", 100);
+    estructura1.invocar();
+
+    Hechisos hechiso1 = Hechisos("Tierras boscosas", "Sombra de intencion");
+    hechiso1.invocar();
+
+    return 0;
+}
