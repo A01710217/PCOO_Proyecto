f55db5d3c2d8fe709b3d72e4e40f272a8c1c4c64
A01710217 <111156446+A01710217@users.noreply.github.com>
2022-11-10 22:58:29 -0600

    Se agrega get y set

--- Proyecto/Estructuras.h
+++ Proyecto/Estructuras.h
@@ -11,18 +11,33 @@ class Estructuras
 
     //Metodos
     public:
-    Estructuras(string, string, int);
-    void invocar();
-    
+        Estructuras(string _tipo, string _nombre, int _vida){
+        tipo = _tipo;
+        nombre = _nombre;
+        vida = _vida;
+        }
+        //Getters
+        string getTipo() {
+            return tipo;
+        }
+        string getNombre() {
+            return nombre;
+        }
+        int getVida() {
+            return vida;
+        }
+        //setters
+        void setTipo(string _tipo) {
+            tipo = _tipo;
+        }
+        void setNombre(string _nombre) {
+            nombre = _nombre;
+        }
+        void setVida(int _vida) {
+            vida = _vida;
+        }
+        //Metodos de la clase
+        void invocar(){
+            cout<<"Estructura de tipo "<<tipo<<endl<<"Nombre: "<<nombre<<endl<<"Vida: "<<vida<<endl;
+        }
 };
-
-Estructuras::Estructuras(string _tipo, string _nombre, int _vida){
-    tipo = _tipo;
-    nombre = _nombre;
-    vida = _vida;
-}
-
-
-void Estructuras::invocar(){
-    cout<<"Estructura de tipo "<<tipo<<endl<<"Nombre: "<<nombre<<endl<<"Vida: "<<vida<<endl;
-}
