10c2d23262680eab1a27a338199e420d936b923d
A01710217 <111156446+A01710217@users.noreply.github.com>
2022-11-28 23:27:26 -0600

    Se crea nueva clase hija de Criaturas.h

---
+++ Proyecto/CriaturaY.h
@@ -0,0 +1,44 @@
+#ifndef CRIATURAY_H
+#define CRIATURAY_H
+
+#include <iostream>
+using namespace std;
+
+#include "Criaturas.h"
+
+class CriaturaY : public Criaturas {
+    //Atributos
+    private:
+    int daño;
+
+    public:
+        //Constructores
+        CriaturaY(string _tipo, string _nombre, string _historia, int _vida, int _ataque, int _danno) :
+        Criaturas(_tipo, _nombre, _historia, _vida, _ataque){
+            daño = _danno;
+        }
+
+        //Getters
+        int getDanno() {
+            return daño;
+        }
+
+        //setters
+        void setDanno(int _danno){
+            daño = _danno;
+        }
+
+        //Metodos de la clase
+        int calcularVida(){
+            return (vida - daño);
+        }
+        void ataque(){
+            cout<<"Estoy atacando"<<endl;
+        }
+        void invocar(){
+            cout<<"He sido invocado"<<endl;
+            cout<<"Soy del "<<tipo<<endl<<"Mi nombre es "<<nombre<<endl<<"Historia: "<<historia<<endl<<"Vida: "<<vida<<endl;
+        }
+};
+
+#endif
