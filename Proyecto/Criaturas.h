063e85bdf9ead2db6905f50819eb0b6b6f1f9fba
A01710217 <111156446+A01710217@users.noreply.github.com>
2022-11-28 23:24:18 -0600

    Avance 3
    Se añadieron constructores y entrega de avance sobre herencia, composición y agregación.

--- Proyecto/Criaturas.h
+++ Proyecto/Criaturas.h
@@ -1,16 +1,30 @@
-#include <string>
+#ifndef CRIATURAS_H
+#define CRIATURAS_H
+
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Criaturas
 {   
     //Atributos
-    private:
+    protected:
         string tipo;
         string nombre;
+        string historia;
         int vida;
+        int ataque;
 
-    //Metodos
     public:
+        //Constructores
+        Criaturas(string _tipo, string _nombre, string _historia, int _vida, int _ataque) {
+            tipo = _tipo;
+            nombre = _nombre;
+            historia = _historia;
+            vida = _vida;
+            ataque = _ataque;
+        }
+
         //Getters
         string getTipo() {
             return tipo;
@@ -18,9 +32,16 @@ class Criaturas
         string getNombre() {
             return nombre;
         }
+        string getHistoria() {
+            return historia;
+        }
         int getVida() {
             return vida;
         }
+        int getAtaque() {
+            return ataque;
+        }
+
         //setters
         void setTipo(string _tipo) {
             tipo = _tipo;
@@ -28,11 +49,21 @@ class Criaturas
         void setNombre(string _nombre) {
             nombre = _nombre;
         }
+        void setHistoria(string _historia){
+            historia = _historia;
+        }
         void setVida(int _vida) {
             vida = _vida;
         }
+        void setAtaque(int _ataque){
+            ataque = _ataque;
+        }
+
         //Metodos de la clase
-        void invocar(){
-            cout<<"Soy del "<<tipo<<endl<<"Mi nombre es "<<nombre<<endl<<"Vida: "<<vida<<endl;
+        virtual int calcularVida() = 0;
+        virtual void invocar(){
+            cout<<"Soy del "<<tipo<<endl<<"Mi nombre es "<<nombre<<endl<<"Vida: "<<vida<<endl<<"Ataque: "<<ataque<<endl;
         }
 };
+
+#endif
