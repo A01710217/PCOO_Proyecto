52a0bb16c99f4938d049d1030a949950ad91e61b
A01710217 <111156446+A01710217@users.noreply.github.com>
2022-12-02 21:15:51 -0600

    Create Hechizo_ataque.h

---
+++ Proyecto/Hechizo_ataque.h
@@ -0,0 +1,41 @@
+#ifndef HECHIZO_ATAQUE_H
+#define HECHIZO_ATAQUE_H
+
+#include <iostream>
+using namespace std;
+
+#include "Hechizo.h"
+
+class Hechizo_ataque : public Hechizo {
+    //Atributos
+    private:
+        int daño;
+
+    public:
+        //Constructores
+        Hechizo_ataque(string _tipo, string _nombre, string _historia, int _danno) :
+        Hechizo(_tipo, _nombre, _historia){
+            daño = _danno;
+        }
+
+        //Getters
+        int getDanno() {
+            return daño;
+        }
+        
+        //setters
+        void setDanno(int _danno) {
+            daño = _danno;
+        }
+        
+        //Metodos de la clase
+        void dannar(){
+            cout<<"Daño"<<endl;
+        }
+        void invocarH(){
+            cout<<"He sido invocado"<<endl;
+            cout<<"Hechizo creado en "<<tipo<<endl<<"Nombre: "<<nombre<<endl<<"Hitoria: "<<historia<<endl<<"Daño: "<<daño<<endl;           
+        }
+};
+
+#endif
